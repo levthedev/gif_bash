64ff1a42b7e2af2da84d4e35ee4e87832e0d669e
Lev Kravinsky <kravinskylev@gmail.com>
2016-04-14 14:47:48 -0400

    rename function, remove self invocation

--- gif.sh
+++ gif.sh
@@ -1,9 +1,7 @@
-function curl_argv() {
+function gif() {
   URL="https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="$@
   PARSE="import json,sys;obj=json.load(sys.stdin);print obj['data']['image_original_url'];"
   curl -s "$URL" | python -c "$PARSE" | xargs curl -s -o .x.gif
   imgcat -w `tput cols` -h `tput lines` .x.gif
   rm .x.gif
 }
-
-curl_argv $@
