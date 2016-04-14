d6c4ead3c7a9b9fa26ca2c419c642ed4aecd8868
Lev Kravinsky <kravinskylev@gmail.com>
2016-04-14 14:45:45 -0400

    move into one function

--- gif.sh
+++ gif.sh
@@ -1,15 +1,9 @@
 function curl_argv() {
   URL="https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="$@
-  curl -s "$URL" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['data']['image_original_url'];"
+  PARSE="import json,sys;obj=json.load(sys.stdin);print obj['data']['image_original_url'];"
+  curl -s "$URL" | python -c "$PARSE" | xargs curl -s -o .x.gif
+  imgcat -w `tput cols` -h `tput lines` .x.gif
+  rm .x.gif
 }
 
-function parse_json() {
-}
-
-function display_gif() {
-  xargs curl -s -o .download.gif
-  imgcat -w `tput cols` -h `tput lines` .download.gif
-  rm .download.gif
-}
-
-curl_argv $@ | display_gif
+curl_argv $@
