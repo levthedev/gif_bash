18b27f619e17897ccc65f2c09de772d026cd5a2b
Lev Kravinsky <kravinskylev@gmail.com>
2016-04-14 16:43:23 -0400

    shorten function

--- gif.sh
+++ gif.sh
@@ -1,6 +1,6 @@
 function gif() {
   URL="https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="$@
-  PARSE="import json,sys;obj=json.load(sys.stdin);print obj['data']['image_original_url'];"
+  PARSE="import json,sys;x=json.load(sys.stdin);print x['data']['image_original_url']"
   curl -s "$URL" | python -c "$PARSE" | xargs curl -s -o .x.gif
   imgcat -w `tput cols` -h `tput lines` .x.gif
   rm .x.gif
